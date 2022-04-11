22857cb3db3abc2923cb767308de8fcebeadd4d0
Beyza Nur Türkü <75912974+beyzaturku@users.noreply.github.com>
2022-04-11 15:37:04 +0300

    Add files via upload

---
+++ HarmonikOrtalamaHesaplama.java
@@ -0,0 +1,31 @@
+
+package harmonikortalamahesaplamaprogrami;
+import java.util.*;
+
+public class HarmonikOrtalamaHesaplamaProgrami {
+
+    public static void main(String[] args) {
+        
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Harmonik Seri Formülü:");
+        System.out.println("1+1/2+1/3+1/4+1/5+.....+1/n");
+        System.out.println("Bir n sayısı giriniz:");
+        int sayi = input.nextInt();
+        
+        double toplam=0.0;
+        double i;
+        for(i=1; i<=sayi; i++)
+        {
+            toplam = toplam + 1.0/i;
+            
+        }
+        
+        System.out.println("Elemanların harmonik seri toplamı:" + toplam );
+        double islem = i/toplam;
+        System.out.print("Harmonik Ortalam Formülü: elemanların sayısı / elemanların harmonik serisi ");
+        System.out.println("Harmonik Ortalama:" + islem);
+
+    }
+    
+}
